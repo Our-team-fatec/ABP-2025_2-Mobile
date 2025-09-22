42e2001359858cb992fa34f827ad19921d762aa5
AliceuJunior <adfj.junior@gmail.com>
2025-09-22 20:15:24 -0300

    Correção para os tests

--- jest.config.js
+++ jest.config.js
@@ -4,9 +4,18 @@ module.exports = {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
   transformIgnorePatterns: [
-    'node_modules/(?!(expo-status-bar|react-native|@react-native|expo|@expo|@unimodules|unimodules|@react-navigation)/)',
+    'node_modules/(?!(expo-status-bar|react-native|@react-native|expo|@expo|@unimodules|unimodules|@react-navigation|expo-router)/)',
   ],
   setupFilesAfterEnv: [
+    '<rootDir>/jest-setup.js',
     '@testing-library/jest-native/extend-expect'
   ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  testMatch: [
+    '**/__tests__/**/*.(ts|tsx|js)',
+    '**/*.(test|spec).(ts|tsx|js)'
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
 };
