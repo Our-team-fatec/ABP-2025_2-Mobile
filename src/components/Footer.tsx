ab126b7c9dea4621d82b745f9fc638c1981a8ee9
lucasdwn <lfcosta0804@gmail.com>
2025-11-17 22:19:33 -0300

    feat: adicionando redirecionamento de tela de listagem de adocao

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -19,6 +19,10 @@ export default function Footer() {
         navigation.navigate("CadastroPet" as never);
     };
 
+    const handleNavigateToAdocaoPet = () => {
+        navigation.navigate("AdocaoPet" as never);
+    };
+
     return (
         <View style={styles.footerWrapper}>
             <SafeAreaView style={styles.safeArea} edges={["bottom"]}>
@@ -35,9 +39,12 @@ export default function Footer() {
                     >
                         <MaterialCommunityIcons name="dog" size={24} color="black" />
                     </TouchableOpacity>
-                    <View style={styles.iconWrapper}>
-                        <Entypo name="heart-outlined" size={24} color="black" style={styles.disabledIcon} />
-                    </View>
+                    <TouchableOpacity 
+                        style={styles.iconWrapper}
+                        onPress={handleNavigateToAdocaoPet}
+                    >
+                        <Entypo name="heart-outlined" size={24} color="black" />
+                    </TouchableOpacity>
                     <View style={styles.iconWrapper}>
                         <FontAwesome name="stethoscope" size={24} color="black" style={styles.disabledIcon} />
                     </View>
